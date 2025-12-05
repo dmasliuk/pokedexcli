22111e9262707bec48b0b96762396bf7994aaeee
dmasliuk <dmasliuk.w@gmail.com>
2025-12-04 23:43:20 -0700

    modified main with basic repl functionality

--- main.go
+++ main.go
@@ -1,11 +1,23 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strings"
 )
 
-
-
 func main() {
-	fmt.Println("Hello, World!")
+	scanner := bufio.NewScanner(os.Stdin)
+	for ;; {
+		pokedexPrompt := "Pokedex > "
+		for fmt.Print(pokedexPrompt); scanner.Scan(); fmt.Print(pokedexPrompt) {
+			input := scanner.Text()
+			inputSlice := cleanInput(input)
+			command := strings.Join(inputSlice[:1], "")
+			input = ""
+			fmt.Printf("Your command was: %s\n", command)
+		}
+	}
+	
 }
